aee33b31c27d44627d638869dd22efdd2805eff8
Louis Samyn <louis.samyn@telenet.be>
2026-01-21 14:09:36 +0100

    Revert super-tab completion settings

--- .config/lazyvim/lua/plugins/blink.cmp.lua
+++ .config/lazyvim/lua/plugins/blink.cmp.lua
@@ -3,16 +3,16 @@ return {
     "saghen/blink.cmp",
     opts = {
       completion = {
-        list = {
-          selection = {
-            preselect = function(ctx)
-              return not require("blink.cmp").snippet_active({ direction = 1 })
-            end,
-          },
-        },
+        -- list = {
+        --   selection = {
+        --     preselect = function(ctx)
+        --       return not require("blink.cmp").snippet_active({ direction = 1 })
+        --     end,
+        --   },
+        -- },
         ghost_text = { enabled = false },
       },
-      keymap = { preset = "super-tab" },
+      -- keymap = { preset = "super-tab" },
     },
   },
 }
