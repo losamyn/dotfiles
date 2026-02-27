02fafe27a0069d94c910d916cb4097bd2e6a3678
Louis Samyn <louis.samyn@telenet.be>
2026-02-27 11:11:57 +0100

    Lazyvim esp32 configuration

---
+++ .config/lazyvim/lua/plugins/esp32.lua
@@ -0,0 +1,12 @@
+return {
+  { "Aietes/esp32.nvim" },
+  {
+    "neovim/nvim-lspconfig",
+    opts = function(_, opts)
+      local esp32 = require("esp32")
+      opts.servers = opts.servers or {}
+      opts.servers.clangd = esp32.lsp_config()
+      return opts
+    end,
+  },
+}
